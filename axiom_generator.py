bbc07c8980a89ab56c730cc48b71fb8586800766
Hazel Griffith <hazelgriffith30@gmail.com>
2025-09-18 18:01:51 -0400

    Added JSON Parsing

--- axiom_generator.py
+++ axiom_generator.py
@@ -1,5 +1,7 @@
 import json
 
-
-a = input("ENTER: ")
-print(a)
+#filename = input("Enter atomic model filename: ")
+filename = 'DEVSMap_Files/counter_atomic.json'
+with open(filename) as jfile:
+    model = json.load(jfile)
+print(model)
