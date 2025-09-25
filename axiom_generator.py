39ce5bff5086cdc5a66bca93497ff7eba85e3d6c
Hazel Griffith <hazelgriffith30@gmail.com>
2025-09-25 12:24:07 -0400

    refactoring script into class incomplete

--- axiom_generator.py
+++ axiom_generator.py
@@ -1,12 +1,68 @@
 import json
 import re
 
-integers = ["int", "unsigned int", "integer", "INT", "unsigned natural", "natural"]
-reals = ["double", "float", "unsigned double", "unsigned float"]
-conditionals = ["==", "!=", ">", "<", ">=", "<=", "&&", "||"]
-conditionalsWOEquals = [">", "<", ">=", "<=", "-", "+", "*", "/"]
-syntaxMap = {"true":"$true", "false":"$false", "==":"=", "<":"$less", "<=":"$lesseq", ">":"$greater", ">=":"$greatereq", "--":"$uminus", "+":"$sum", "-":"$difference", "*":"product", "/":"$quotient", "&&":"&", "||":"|"}
 
+class Axiom_Generator:
+    integers = ["int", "unsigned int", "integer", "INT", "unsigned natural", "natural"]
+    reals = ["double", "float", "unsigned double", "unsigned float"]
+    conditionals = ["==", "!=", ">", "<", ">=", "<=", "&&", "||"]
+    conditionalsWOEquals = [">", "<", ">=", "<=", "-", "+", "*", "/"]
+    syntaxMap = {"true":"$true", "false":"$false", "==":"=", "<":"$less", "<=":"$lesseq", ">":"$greater", ">=":"$greatereq", "--":"$uminus", "+":"$sum", "-":"$difference", "*":"product", "/":"$quotient", "&&":"&", "||":"|"}
+
+
+    def __init__(self, model_file_path: str, init_state_file_path: str):
+        #Getting atomic model file
+        with open(model_file_path) as jfile:
+            self.model_json = json.load(jfile)["counter"]
+
+        #Getting initial state file
+        with open(init_state_file_path) as jfile:
+            self.init_state_json = json.load(jfile)
+
+        #initializing variables
+        self.tff_string = ''
+        self.state_var_names = []
+        self.in_port_names = []
+        self.out_port_names = []
+
+    '''
+    Creates a new .p file with the given name containing the tff axioms generated
+    args: filename = string
+    '''
+    def save(self, filename:str):
+        try:
+            with open(filename+".p", 'x') as pfile:
+                pfile.write(self.tff_string)
+
+        except Exception as e:
+            print(e)
+
+    '''
+    Parses the JSON dictionary of the state variables and saves the tff statements in the class string
+    '''
+    def parse_state_vars(self):
+        state = model["s"]
+    
+        self.tff_string+=("%-----STATE VARIABLE DEFINITIONS\n")
+        for pos, key in enumerate(state):
+            self.state_var_names.append(key)
+            line = "tff("+key+", type, "
+            if (state[key] in integers):
+                line+= "$int)."
+            elif (state[key] == "bool"):
+                line+= "$o)."
+            elif (state[key] in reals):
+                line+= "$real)."
+            self.tff_string+=(line+"\n")
+            line = "tff(next_"+key+", type, "
+            if (state[key] in integers):
+                line+= "$int)."
+            elif (state[key] == "bool"):
+                line+= "$o)."
+            elif (state[key] in reals):
+                line+= "$real)."
+            self.tff_string+=(line+"\n")
+    
 def parseDict_recurs(deltaDict, line):
     for pos, key in enumerate(deltaDict):
         newline = ""
@@ -45,20 +101,14 @@ def parseDict_recurs(deltaDict, line):
     return line+newline
                     
 
-#filename = input("Enter atomic model filename: ")
-filename = 'DEVSMap_Files/counter_atomic.json'
-with open(filename) as jfile:
-    model = json.load(jfile)["counter"]
 
-filename = 'DEVSMap_Files/counter_tester_init_state.json'
-with open(filename) as jfile:
-    init_state = json.load(jfile)
 
-with open("test.p", "x") as pfile:
+
     state = model["s"]
     
     pfile.write("%-----STATE VARIABLE DEFINITIONS\n")
     for pos, key in enumerate(state):
+        state_var_names.append(key)
         line = "tff("+key+", type, "
         if (state[key] in integers):
             line+= "$int)."
@@ -84,6 +134,7 @@ with open("test.p", "x") as pfile:
     pfile.write("tff(i_port_type, type, i_port : $tType).\n")
     define_set = "tff(only_i_ports, axiom,\n\t! [IP : i_port] :\n\t\t( "
     for pos, key in enumerate(in_ports):
+        in_port_names.append(key)
         pfile.write("tff("+key+", type, "+key+" : i_port).\n")
         line = "tff(rcvd_"+key+", type, rcvd_"+key+" : "
         if (in_ports[key] in integers):
@@ -106,6 +157,7 @@ with open("test.p", "x") as pfile:
     pfile.write("tff(o_port_type, type, o_port : $tType).\n")
     define_set = "tff(only_o_ports, axiom,\n\t! [OP : o_port] :\n\t\t( "
     for pos, key in enumerate(out_ports):
+        out_port_names.append(key)
         pfile.write("tff("+key+", type, "+key+" : o_port).\n")
         define_set += "OP = "+key
         if (pos+1 != len(out_ports)):
@@ -118,6 +170,8 @@ with open("test.p", "x") as pfile:
 
     pfile.write("%-----INTERNAL TRANSITIONS\n")
 
+    #Flatten the nested dictionary of conditions and state variable assignments
+    #For the internal transition function
     flat_delta_int = {}
     stack = [(delta_int, '')]
 
@@ -127,16 +181,27 @@ with open("test.p", "x") as pfile:
         strings = []
         for k, v in c.items():
             
-            
             if isinstance(v, dict):
-                new_key = f"{p}_{k}" if p else k
-                stack.append((v, new_key)) 
+                if len(v) > 0:
+                    new_key = f"{p};{k}" if p else k
+                    stack.append((v, new_key)) 
+                else:
+                    strings.append(k)
             else:
                 strings.append(k+ " = "+v)
         if (len(strings) > 0):
-            flat_delta_int[p] = strings
-        
-    print(flat_delta_int)
+            if (p == ''):
+                flat_delta_int['otherwise'] = 'True'
+            else:
+                flat_delta_int[p] = strings
     
+    for pos, key in enumerate(flat_delta_int):
+        line = "tff(delta_int_"+str(pos)+",axiom,\n"
+        conds = key.split(";")
+        assert(len(conds > 0))
+        if (len(conds)==1):
 
-        
+if __name__ == '__main___':
+    axiom_gen = Axiom_Generator('DEVSMap_Files/counter_atomic.json', 'DEVSMap_Files/counter_tester_init_state.json')
+
+    axiom_gen.save("test")
